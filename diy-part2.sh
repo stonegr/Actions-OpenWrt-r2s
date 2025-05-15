d99999c5dceb9ce8925a9fd1384d24da5b6f290d
stonegr <eloisasethxdg62@gmail.com>
2025-05-15 12:28:23 +0800

    修改ip，主机名，添加kmod-ifb支持

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.1.1/192.168.100.180/g' package/base-files/files/bin/config_gen
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/Stone/g' package/base-files/files/bin/config_generate
